41aa043678a42e64f68a2338de248727039dba3a
Philip Werner <phiwer@gmail.com>
2023-06-17 15:58:31 +0200

    Initial commit

---
+++ football-matches-api/src/main/java/com/phiwer/footballmatchesapi/FootballMatchesApiApplication.java
@@ -0,0 +1,13 @@
+package com.phiwer.footballmatchesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballMatchesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballMatchesApiApplication.class, args);
+    }
+
+}
---
+++ football-matches-api/src/test/java/com/phiwer/footballmatchesapi/FootballMatchesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phiwer.footballmatchesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballMatchesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
